264d7b0e0766abdf73739fbefd41010921a6f75d
Konrad Putelbergier <Konrad.Putelbergier@gmail.com>
2023-06-20 14:13:52 +0200

    Zrobione zadanie 5.1

--- src/main/java/pl/javastart/task/CoordinateSystem.java
+++ src/main/java/pl/javastart/task/CoordinateSystem.java
@@ -6,6 +6,25 @@ public class CoordinateSystem {
 
     // uzupełnij metodę. Używaj scannera przekazanego w parametrze.
     void run(Scanner scanner) {
-        System.out.println("Punkt (5, -3)");
+        System.out.println("Podaj wspolrzędna x punktu:");
+        int x = scanner.nextInt();
+        System.out.println("Podaj wspolrzędna y punktu:");
+        int y = scanner.nextInt();
+        Point point = new Point(x, y);
+        if (x < 0 && y > 0) {
+            System.out.println("Punkt (" + point.getX() + ", " + point.getY() + ") leży w I ćwiartce układu współrzędnych");
+        } else if (x > 0 && y > 0) {
+            System.out.println("Punkt (" + point.getX() + ", " + point.getY() + ") leży w II ćwiartce układu współrzędnych");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Punkt (" + point.getX() + ", " + point.getY() + ") leży w III ćwiartce układu współrzędnych");
+        } else if (x > 0 && y < 0) {
+            System.out.println("Punkt (" + point.getX() + ", " + point.getY() + ") leży w IV ćwiartce układu współrzędnych");
+        } else if (x == 0 && y == 0) {
+            System.out.println("Punkt (" + point.getX() + ", " + point.getY() + ") leży w środku układu współrzędnych");
+        } else if (x == 0 && y != 0) {
+            System.out.println("Punkt (" + point.getX() + ", " + point.getY() + ") leży na osi X");
+        } else if (x != 0 && y == 0) {
+            System.out.println("Punkt (" + point.getX() + ", " + point.getY() + ") leży na osi Y");
+        }
     }
 }
